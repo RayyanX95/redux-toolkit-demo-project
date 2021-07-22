8462485c1ab7a0eb038defcd535a4c10f950c3f6
Ibarhim <ibrahim.alrayany@gmail.com>
2021-07-22 15:19:42 +0200

    redux toolkit demo project

---
+++ src/store/auth.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: { isAuthenticated: false },
+  reducers: {
+    login(state, action) {
+      state.isAuthenticated = true;
+    },
+    logout(state, action) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export const authActions = authSlice.actions;
+
+
+export default authSlice;
